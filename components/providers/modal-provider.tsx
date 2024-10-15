ab0afec15b9c34a2047a3404c418a7f729de28a6
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-15 15:36:57 -0500

    Server Actions Complete

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,11 @@
 import { useEffect, useState } from "react"
 import { CreateServerModal } from "@/components/modals/create-server-modal"
 import { InviteModal } from "../modals/invite-modal"
+import { EditServerModal } from "../modals/edit-server-modal"
+import { MembersModal } from "../modals/members-modal"
+import { CreateChannelModal } from "../modals/create-channel-modal"
+import { LeaveServerModal } from "../modals/leave-server-modal"
+import { DeleteServerModal } from "../modals/delete-server-modal"
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false)
@@ -19,9 +24,14 @@ export const ModalProvider = () => {
     console.log(isMounted)
 
     return (
-        <div>
+        <>
             <CreateServerModal />
             <InviteModal />
-        </div>
+            <EditServerModal />
+            <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
+        </>
     )
 }
