0c2cb66753b9041907948979e3d016283df07484
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-16 16:15:21 -0500

    add socket.io provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -30,10 +31,12 @@ export default function RootLayout({
             defaultTheme="dark"
             enableSystem
             storageKey="discord-clone-theme"
-          >
+            >
+            <SocketProvider>
             
-            {children}
             <ModalProvider />
+            {children}
+          </SocketProvider>
           </ThemeProvider>
         </body>
       </ClerkProvider>
