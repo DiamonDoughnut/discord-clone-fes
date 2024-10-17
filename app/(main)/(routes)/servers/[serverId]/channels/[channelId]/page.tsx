2bc25c833140da948ed703522fb4ad9e29f7e89b
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-16 20:32:20 -0500

    implement rendering of chat messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
@@ -41,8 +42,21 @@ const ChannelIdPage = async ({
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
-            <div className="flex-1">
-                Future Messages
+            <div className="flex-1 flex flex-col">
+                <ChatMessages  
+                    member={member}
+                    name={channel.name}
+                    chatId={channel.id}
+                    type="channel"
+                    apiUrl='/api/messages'
+                    socketUrl="/api/socket/messages"
+                    socketQuery={{
+                        channelId: channel.id,
+                        serverId: channel.serverId
+                    }}
+                    paramKey="channelId"
+                    paramValue={channel.id}
+                />
             </div>
             <ChatInput name={channel.name} type='channel' apiUrl="/api/socket/messages" query={{
                 channelId: channel.id,
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -20,23 +21,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
       <ClerkProvider>
-        <body className={`${cn(
-          font.className,
-          'bg-white dark:bg-[#313338]'
-        )} antialiased`}>
+        <body
+          className={`${cn(
+            font.className,
+            "bg-white dark:bg-[#313338]"
+          )} antialiased`}
+        >
           <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
+            attribute='class'
+            defaultTheme='dark'
             enableSystem
-            storageKey="discord-clone-theme"
-            >
+            storageKey='discord-clone-theme'
+          >
             <SocketProvider>
-            
-            <ModalProvider />
-            {children}
-          </SocketProvider>
+              <QueryProvider>
+                <ModalProvider />
+                {children}
+              </QueryProvider>
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </ClerkProvider>
