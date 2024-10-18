430d05c6d4a7f2fea58970543fe3ce06e3bc79f9
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-17 23:06:43 -0500

    delete messages and real time web-socket updates

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -3,6 +3,7 @@
 import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
@@ -40,6 +41,8 @@ export const ChatMessages = ({
     type
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`
+    const updateKey = `chat:${chatId}:messages:update`
 
     const {
         data,
@@ -53,6 +56,7 @@ export const ChatMessages = ({
         paramKey,
         paramValue
     }); 
+    useChatSocket({ queryKey, addKey, updateKey})
 
     if (status === 'loading') {
         return (
