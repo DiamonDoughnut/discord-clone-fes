66125f43a81358279e8df059aed6a4179598ec4c
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-18 00:36:56 -0500

    final commit for deploy, needs testing when db reopens

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,69 +1,96 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
 import { ChatMessages } from "@/components/chat/chat-messages";
-import { currentProfile } from "@/lib/current-profile"
+import { MediaRoom } from "@/components/media-room";
+import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 interface ChannleIdPageProps {
-    params: {
-        serverId: string,
-        channelId: string
-    }
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
 
-const ChannelIdPage = async ({
-    params
-}: ChannleIdPageProps) => {
-    const profile = await currentProfile();
+const ChannelIdPage = async ({ params }: ChannleIdPageProps) => {
+  const profile = await currentProfile();
 
-    if (!profile) {
-        return auth().redirectToSignIn();
-    }
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
 
-    const channel = await db.channel.findUnique({
-        where: {
-            id: params.channelId
-        }
-    })
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
 
-    const member = await db.member.findFirst({
-        where: {
-            serverId: params.serverId,
-            profileId: profile.id
-        }
-    })
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
 
-    if (!channel || !member) {
-        redirect('/')
-    }
-    
-    return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-            <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
-            <div className="flex-1 flex flex-col">
-                <ChatMessages  
-                    member={member}
-                    name={channel.name}
-                    chatId={channel.id}
-                    type="channel"
-                    apiUrl='/api/messages'
-                    socketUrl="/api/socket/messages"
-                    socketQuery={{
-                        channelId: channel.id,
-                        serverId: channel.serverId
-                    }}
-                    paramKey="channelId"
-                    paramValue={channel.id}
-                />
-            </div>
-            <ChatInput name={channel.name} type='channel' apiUrl="/api/socket/messages" query={{
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type='channel'
+      />
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <div className='flex-1 flex flex-col'>
+            <ChatMessages
+              member={member}
+              name={channel.name}
+              chatId={channel.id}
+              type='channel'
+              apiUrl='/api/messages'
+              socketUrl='/api/socket/messages'
+              socketQuery={{
                 channelId: channel.id,
-                serverId: channel.serverId
-            }} />
-        </div>
-    )
-}
+                serverId: channel.serverId,
+              }}
+              paramKey='channelId'
+              paramValue={channel.id}
+            />
+          </div>
+          <ChatInput
+            name={channel.name}
+            type='channel'
+            apiUrl='/api/socket/messages'
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
+      {channel.type === ChannelType.AUDIO && (
+        <MediaRoom
+            chatId={channel.id}
+            video={false}
+            audio={true}
+        />
+      )}
+      {channel.type === ChannelType.VIDEO && (
+        <MediaRoom
+            chatId={channel.id}
+            video={true}
+            audio={true}
+        />
+      )}
+    </div>
+  );
+};
 
-export default ChannelIdPage
+export default ChannelIdPage;
